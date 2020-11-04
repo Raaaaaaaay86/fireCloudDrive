492ee0ea7c3d7a128ff12fd3c8b87940597ec705
Raaaaaaaay86 <mirror46258@gmail.com>
2020-11-04 11:29:52 +0800

    修改變數設定

--- api/connection/firebaseAdmin.js
+++ api/connection/firebaseAdmin.js
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
       type: process.env.ADMIN__TYPE,
       project_id: process.env.ADMIN_PROJECT_ID,
       private_key_id: process.env.ADMIN_PRIVATE_KEY_ID,
-      private_key: process.env.ADMIN_PRIVATE_KEY,
+      private_key: process.env.ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
       client_email: process.env.ADMIN_CLIENT_EMAIL,
       client_id: process.env.ADMIN_CLIENT_ID,
       auth_uri: process.env.ADMIN_AUTH_URI,
